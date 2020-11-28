89c6846dcae10b95e723eb9223f80b24b6bd12fe
BlackSwan-Decoded <aronaws@hotmail.com>
2020-11-27 23:53:37 -0500

    user can successfully log in

---
+++ app/src/main/java/com/example/myloop/ParseApplication.java
@@ -0,0 +1,20 @@
+package com.example.myloop;
+
+import android.app.Application;
+
+import com.parse.Parse;
+
+public class ParseApplication extends Application {
+
+    @Override
+    public void onCreate() {
+        super.onCreate();
+
+        Parse.initialize(new Parse.Configuration.Builder(this)
+                .applicationId("CGmyrRenhRaFUxkoejDtMWptyuLzB355Rqp8bcd1")
+                .clientKey("CsvyOT7jWDUv4X0Pp2X3njtiiSaUHgvpouduvosG")
+                .server("https://parseapi.back4app.com")
+                .build()
+        );
+    }
+}
