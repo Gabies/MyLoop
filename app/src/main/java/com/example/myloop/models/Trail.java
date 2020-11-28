eee1086960a4b5deb2f4c463380b6f37a48b94d0
iamGabby <gabiseef@gmail.com>
2020-11-27 22:25:36 -0500

    parse setup

---
+++ app/src/main/java/com/example/myloop/models/Trail.java
@@ -0,0 +1,14 @@
+package com.example.myloop.models;
+
+import com.parse.ParseClassName;
+import com.parse.ParseObject;
+
+@ParseClassName("Trail")
+public class Trail extends ParseObject {
+    public static final String ROUTE = "Route";
+    public static final String TRAIL_NAME = "TrailName";
+    public static final String TRAIL_DISTANNCE = "Length";
+
+
+
+}
