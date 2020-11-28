583631d1e131e767330de38ccc7b3227bb0343e3
iamGabby <gabiseef@gmail.com>
2020-11-28 00:12:28 -0500

    Data Models created

--- app/src/main/java/com/example/myloop/models/Trail.java
+++ app/src/main/java/com/example/myloop/models/Trail.java
@@ -1,14 +1,29 @@
 package com.example.myloop.models;
 
+import com.parse.Parse;
 import com.parse.ParseClassName;
+import com.parse.ParseFile;
 import com.parse.ParseObject;
+import com.parse.ParsePolygon;
+import com.parse.ParseUser;
 
 @ParseClassName("Trail")
 public class Trail extends ParseObject {
     public static final String ROUTE = "Route";
     public static final String TRAIL_NAME = "TrailName";
     public static final String TRAIL_DISTANNCE = "Length";
+    public static final String TRAIL_SCENERY = "Scenery";
 
+    public ParseUser getTrail() {
+        return getParseUser(TRAIL_NAME);
+    }
 
+    public void setTrail(ParseUser user) { put(TRAIL_NAME, user); }
+
+    public ParsePolygon getTrailRoute() { return getParsePolygon(ROUTE); }
+
+    public double getTrailDistance() { return getDouble(TRAIL_DISTANNCE); }
+
+    public ParseFile getTrailScenery() { return getParseFile(TRAIL_SCENERY); }
 
 }
