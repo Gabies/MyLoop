5ce38d78ca7dedb2c2cceebce969c06aded9b849
BlackSwan-Decoded <aronaws@hotmail.com>
2020-12-01 22:31:38 -0500

    user can logout of the app

--- app/src/main/java/com/example/myloop/MainActivity.java
+++ app/src/main/java/com/example/myloop/MainActivity.java
@@ -2,13 +2,37 @@ package com.example.myloop;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.util.Log;
+import android.view.View;
+import android.widget.Button;
+
+import com.parse.ParseUser;
 
 public class MainActivity extends AppCompatActivity {
 
+    public static final String TAG = "MainActivity";
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        Button logoutButton = findViewById(R.id.logoutButton);
+        logoutButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Log.i(TAG, "Logout button clicked");
+                ParseUser.logOut();
+                ParseUser currentUser = ParseUser.getCurrentUser();
+
+                goLogin();
+            }
+        });
+    }
+
+    private void goLogin() {
+        Intent i = new Intent(this, LoginActivity.class);
+        startActivity(i);
     }
 }
---
+++ app/src/main/java/com/example/myloop/models/RegisterActivity.java
@@ -0,0 +1,16 @@
+package com.example.myloop.models;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+import com.example.myloop.R;
+
+public class RegisterActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_register);
+    }
+}
