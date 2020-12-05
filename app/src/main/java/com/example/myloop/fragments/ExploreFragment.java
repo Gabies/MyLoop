536b62b27412acb343fa1b931071b382098443b2
rtusiime <rtusiime@u.rochester.edu>
2020-12-05 01:29:12 -0500

    updated fragment screate fragment layout

--- app/src/main/java/com/example/myloop/fragments/ExploreFragment.java
+++ app/src/main/java/com/example/myloop/fragments/ExploreFragment.java
@@ -1,8 +1,27 @@
 package com.example.myloop.fragments;
 
+import android.os.Bundle;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
 import androidx.fragment.app.Fragment;
 
+import com.example.myloop.R;
+
 public class ExploreFragment extends Fragment {
+    public ExploreFragment() {
+        // Required empty public constructor
+    }
+
+    @Nullable
+    @Override
+    public View onCreateView(@NonNull LayoutInflater inflater, @Nullable ViewGroup container,
+                             @Nullable Bundle savedInstanceState) {
+        return inflater.inflate(R.layout.fragment_explore, container, false);
+    }
 
 
 }
